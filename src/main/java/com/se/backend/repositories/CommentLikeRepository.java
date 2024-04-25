acd42ebfc7e92a6ba9d5de96519f819a4086d615
ableyyyx <2724475986@qq.com>
2024-04-25 11:01:57 +0800

    feat: add comment like

--- src/main/java/com/se/backend/repositories/CommentLikeRepository.java
+++ src/main/java/com/se/backend/repositories/CommentLikeRepository.java
@@ -5,6 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long>, JpaSpecificationExecutor<CommentLike> {
+    List<CommentLike> findAllByCommentId(Long commentId);
+
+    List<CommentLike> findAllByUserId(Long userId);
+
+    List<CommentLike> findByUserIdAndCommentId(Long userId, Long commentId);
 }
