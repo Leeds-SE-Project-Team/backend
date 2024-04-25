445a0bba53b068bb2b43dfb856e666870b2b178c
ableyyyx <2724475986@qq.com>
2024-04-25 16:35:29 +0800

    fix: update exception message

--- src/main/java/com/se/backend/exceptions/ResourceException.java
+++ src/main/java/com/se/backend/exceptions/ResourceException.java
@@ -31,10 +31,10 @@ public class ResourceException extends Exception {
         GROUP_NOT_FOUND("Group not found"), GROUP_COLLECTION_NOT_FOUND("Group collection not found"),
 
         TOUR_LIKE_NOT_FOUND("Tour like not found"), TOUR_STAR_NOT_FOUND("Tour star not found"),
-        TOUR_LIKE_EXISTS("tour has been liked"),TOUR_STAR_EXISTS("tour has been starred"),
+        TOUR_LIKE_EXISTS("tour can not be like twice"),TOUR_STAR_EXISTS("tour can not be star twice"),
 
         COMMENT_NOT_FOUND("Comment not found"), COMMENT_LIKE_NOT_FOUND("Comment like not found"),
-        COMMENT_LIKE_EXISTS("comment has been liked");
+        COMMENT_LIKE_EXISTS("comment can not be like twice");
         private final String message;
 
         ErrorType(String message) {
